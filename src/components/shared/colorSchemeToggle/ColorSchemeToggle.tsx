3d6192599ce9983fed778e738aa332dc29c50404
codeBelt <robertsavian@gmail.com>
2025-04-17 16:48:55 -0500

    Add Shadcn UI

--- src/components/shared/colorSchemeToggle/ColorSchemeToggle.tsx
+++ src/components/shared/colorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,46 +1,14 @@
 'use client';
+import {Button} from '@/components/ui/shadcn/button';
 import {useColorScheme} from '@/hooks/useColorScheme/useColorScheme';
-import {useIsClient} from '@uidotdev/usehooks';
+import {Moon, Sun} from 'lucide-react';
 
-export const ColorSchemeToggle = () => {
-  const isClient = useIsClient();
-  const {scheme, toggle, setDark, setLight, respectSystem} = useColorScheme();
+export function ColorSchemeToggle() {
+  const {toggle, scheme} = useColorScheme();
 
   return (
-    <div className="mx-auto max-w-md space-y-4 rounded bg-white p-6 text-gray-800 shadow dark:bg-gray-900 dark:text-white">
-      <p className="font-semibold text-lg">
-        Current Scheme: {isClient ? <span className="capitalize">{scheme}</span> : null}
-      </p>
-      <div className="flex flex-wrap gap-4">
-        <button
-          onClick={toggle}
-          className="rounded bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
-          type={'button'}
-        >
-          Toggle Scheme
-        </button>
-        <button
-          onClick={setDark}
-          className="rounded bg-gray-800 px-4 py-2 text-white transition hover:bg-gray-900"
-          type={'button'}
-        >
-          Force Dark
-        </button>
-        <button
-          onClick={setLight}
-          className="rounded bg-yellow-400 px-4 py-2 text-black transition hover:bg-yellow-500"
-          type={'button'}
-        >
-          Force Light
-        </button>
-        <button
-          onClick={respectSystem}
-          className="rounded bg-green-600 px-4 py-2 text-white transition hover:bg-green-700"
-          type={'button'}
-        >
-          Use System Preference
-        </button>
-      </div>
-    </div>
+    <Button variant="ghost" size="icon" onClick={toggle}>
+      {scheme === 'dark' ? <Sun /> : <Moon />}
+    </Button>
   );
-};
+}
